17381fc0f46fd5920d9a27283032b9b460298f2b
Gerardo Rivera Tello <gerardo_art@me.com>
2021-06-29 11:52:32 -0500

    Add docstrings

--- nmme_tools/postprocess.py
+++ nmme_tools/postprocess.py
@@ -1,7 +1,14 @@
+"""
+Postprocessing module that morphs the data structures in convenient ways
+"""
+
 import xarray as xr
 
 
 def combine_hindcast_forecast(hindcast, forecast, propagate_nans=True):
+    """
+    Combine the hindcast and forecast datasets into a single xarray.Dataset
+    """
     nan_mask = 1
     if propagate_nans:
         hindcast_mask = hindcast.isel(time=0, drop=True).isnull()
--- nmme_tools/preprocess.py
+++ nmme_tools/preprocess.py
@@ -1,18 +1,33 @@
+"""
+Preprocess module with utilities that aim to uniformize the
+naming convention and some other dimension/coordinate operations.
+
+Does not modify the underlying data
+"""
 import numpy as np
 import xarray as xr
 
 
 def rename_dims(ds):
+    """
+    Rename dataset dimensions with a more explicit naming scheme
+    """
     dims_mapping = {"L": "lead", "Y": "lat", "X": "lon", "M": "member", "S": "time"}
     return ds.rename({k: v for k, v in dims_mapping.items() if k in ds.variables})
 
 
 def fix_time(ds):
+    """
+    Fix non-standard calendar to work with cftime
+    """
     ds["time"].attrs["calendar"] = "360_day"
     return xr.decode_cf(ds)
 
 
 def coords_to_int(ds, coords):
+    """
+    Cast the coordinates to integer values
+    """
     coords = [coords] if not isinstance(coords, list) else coords
     for coord in coords:
         _coord_attrs = ds[coord].attrs
@@ -22,6 +37,9 @@ def coords_to_int(ds, coords):
 
 
 def capitalize_names(ds):
+    """
+    Capitalize the 'long_name' attribute for plotting purposes
+    """
     for var in ds.variables.values():
         if "long_name" in var.attrs:
             var.attrs["long_name"] = var.attrs["long_name"].capitalize()
@@ -29,6 +47,10 @@ def capitalize_names(ds):
 
 
 def preprocess_nmme(ds):
+    """
+    Core preprocessing function. It combines most of the standalone functions
+    defined in nmme_tools.preprocess to provide a unified tool.
+    """
     ds = rename_dims(ds)
     ds = fix_time(ds)
     coords = [coord for coord in ["lead", "member"] if coord in ds.variables]
