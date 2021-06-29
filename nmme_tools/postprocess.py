83e387e0e8e5d317da64f590f20206c28cc7ec24
Gerardo Rivera Tello <gerardo_art@me.com>
2021-06-29 13:40:51 -0500

    Use notnull instead of isnull

--- nmme_tools/postprocess.py
+++ nmme_tools/postprocess.py
@@ -11,8 +11,8 @@ def combine_hindcast_forecast(hindcast, forecast, propagate_nans=True):
     """
     nan_mask = 1
     if propagate_nans:
-        hindcast_mask = hindcast.isel(time=0, drop=True).isnull()
-        forecast_mask = forecast.isel(time=0, drop=True).isnull()
+        hindcast_mask = hindcast.isel(time=0, drop=True).notnull()
+        forecast_mask = forecast.isel(time=0, drop=True).notnull()
         nan_mask = hindcast_mask * forecast_mask
-    merged = xr.concat([hindcast, forecast], dim="time", join="left").where(~nan_mask)
+    merged = xr.concat([hindcast, forecast], dim="time", join="left").where(nan_mask)
     return merged
