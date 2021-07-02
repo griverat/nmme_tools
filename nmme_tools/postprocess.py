ee4792a22f13b85db2c8aaca9624d72ebd89b204
Gerardo Rivera Tello <gerardo_art@me.com>
2021-07-01 23:14:34 -0500

    Ignore dask warning

--- nmme_tools/postprocess.py
+++ nmme_tools/postprocess.py
@@ -2,7 +2,7 @@
 Postprocessing module that morphs the data structures in convenient ways
 """
 
-import xarray as xr
+import dask
 
 
 def combine_hindcast_forecast(hindcast, forecast, propagate_nans=True):
@@ -14,8 +14,9 @@ def combine_hindcast_forecast(hindcast, forecast, propagate_nans=True):
         hindcast_mask = hindcast.isel(time=0, drop=True).notnull()
         forecast_mask = forecast.isel(time=0, drop=True).notnull()
         nan_mask = hindcast_mask * forecast_mask
-    merged = xr.concat([hindcast, forecast], dim="time", join="left").where(nan_mask)
-    return merged
+    with dask.config.set({"array.slicing.split_large_chunks": False}):
+        _res = hindcast.combine_first(forecast).where(nan_mask)
+    return _res
 
 
 def mask_data(ds, mask_mapping):
